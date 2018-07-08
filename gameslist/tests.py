501eb4f60f9b00582a12e2165d1e479b24928e7d
Morgan Racine <jracine@bu.edu>
2018-07-08 17:11:01 +0000

    Update tests.py with some notes

--- gameslist/tests.py
+++ gameslist/tests.py
@@ -15,6 +15,26 @@ from django.test import TestCase
 #priority calculations
 #aging 0 when beaten/abandoned?
 #current time fetches?
+#logging time
+#fetching current time (steam?)
+#deleting a game
+#lending a game
+#recommend a game (check priroity)
+#pass over a game
+#searching for types of games (lots of tests of different searchs)
+    #ie, by platform, by played, by recommended etc
+
+#ratings
+#rate a as better than b
+#rate a as more desireed (wl) than b
+#check priorities based on that
+#make sure only grabs 5 paris to compare
+#check grabs valid pairs to compare
+#what are the comparison rules:
+  #less  ratings > more ratings 
+  # for owned: beaten > played > unplayed > abandoned
+  #for owned: 
+  #for wished: older > newer
 
 #humble wishlist fetch
 #itch wishlist fetch
