4b95a0547c80166eee9559226b5f46d069cbc36d
James Racine <jracine@bullhorn.com>
2018-10-30 20:18:55 -0400

    Settings, not really good for prod yet

--- games_list/settings.py
+++ games_list/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nmt-gpp(iwo=fpnf0*tf0=d@gxyhlt8xxpm2$!7ggj-9ixt851'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['games-test.rw3dvkmeac.us-west-2.elasticbeanstalk.com',
+                 'gameslist.griffonflightproductions.com',
                  '127.0.0.1']
 
 
