c8f1c443489719e55400c8f451ee2fd6580ffede
James Racine <jracine@bullhorn.com>
2018-10-31 12:02:50 -0400

    GL-11 debug debug debug

--- games_list/settings.py
+++ games_list/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'nmt-gpp(iwo=fpnf0*tf0=d@gxyhlt8xxpm2$!7ggj-9ixt851'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ADMINS = [('Morgan','griffonlord@gmail.com')]
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['games-test.rw3dvkmeac.us-west-2.elasticbeanstalk.com',
                  'gameslist.griffonflightproductions.com',
